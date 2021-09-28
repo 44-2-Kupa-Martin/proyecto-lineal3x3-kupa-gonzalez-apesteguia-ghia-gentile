cd9ee2a5c262443d23277c0b05372db46aa63742
44-2-Kupa-Martin <kupamartinclassroom@gmail.com>
2021-09-28 20:30:02 -0300

    -

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 #index.py
 import sys
-x= lambda a: float(a)
-multiples= False
-
+#Function declarations
 def inputHandler(str):
+    x= lambda a: float(a)
     try:
         if (str.find(',')== -1):
             return tuple(map(x, str.split(' ')))
@@ -11,12 +10,12 @@ def inputHandler(str):
             return tuple(map(x, str.replace(' ', '').split(',')))
     except:
         sys.exit('Invalid inputs')
-
+#Global vars
+multiples= False
 e1 = inputHandler(input('Enter the 1st equation\n(e.g 1,2,3)\n'))
 e2 = inputHandler(input('Enter the 2st equation\n'))
 e3 = inputHandler(input('Enter the 3st equation\n'))
-
-
+#System type checks
 if((e1[1]*e3[0]-e1[0]*e3[1])*(e1[2]*e2[0]-e1[0]*e2[2])-(e1[1]*e2[0]-e1[0]*e2[1])*(e1[2]*e3[0]-e1[0]*e3[2]) == 0):
     if(e1[0] / e2[0] == e1[1] / e2[1] == e1[2] / e2[2]):
         print('The 1st and the 2nd equation are multiples')

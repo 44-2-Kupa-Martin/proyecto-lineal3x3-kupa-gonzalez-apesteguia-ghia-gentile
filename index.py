d66b9b45ea1562989507c7a7f2c25e045251b7b4
44-2-Kupa-Martin <kupamartinclassroom@gmail.com>
2021-09-30 21:34:56 -0300

    division by zero (2/2)

--- index.py
+++ index.py
@@ -26,12 +26,61 @@ def inputHandler(str):
     except:
         sys.exit('Invalid inputs')
 
+def preventDivByZero(e1, e2, e3):
+    check= [['e1[0] / e2[0]', 'e1[1] / e2[1]', 'e1[2] / e2[2]'],['e2[0] / e3[0]', 'e2[1] / e3[1]', 'e2[2] / e3[2]'],['e1[0] / e3[0]', 'e1[1] / e3[1]', 'e1[2] / e3[2]']]
+    e1e2Statement= None
+    e2e3Statement= None
+    e1e3Statement= None
+    #test if values are suitable for division
+    for index in range(3):
+        #checks for e2[]
+        if (e2[index]==0):
+            if (e1[index]==0):
+                check[0].pop(index)
+            else:
+                check[0]= False
+        #checks for e3[]
+        if (e3[index]==0):
+            if (e2[index]==0):
+                check[1].pop(index)
+            else:
+                check[1]= False
+            if (e1[index]==0):
+                check[2].pop(index)
+            else:
+                check[2]= False
+    #statement constructor
+    if (check[0]):
+        check0Length= len(check[0])
+        if (check0Length == 1):
+            e1e2Statement= True    
+        elif (check0Length == 2):
+            e1e2Statement= eval(f'{check[0][0]} == {check[0][1]}')
+        elif (check0Length == 3):
+            e1e2Statement= eval(f'{check[0][0]} == {check[0][1]} == {check[0][2]}')
+    if (check[1]):
+        check1Length= len(check[1])
+        if (check1Length == 1):
+            e2e3Statement= True    
+        elif (check1Length == 2):
+            e2e3Statement= eval(f'{check[1][0]} == {check[1][1]}')
+        elif (check1Length == 3):
+            e2e3Statement= eval(f'{check[1][0]} == {check[1][1]} == {check[1][2]}')
+    if (check[2]):
+        check2Length= len(check[2])
+        if (check2Length == 1):
+            e1e3Statement= True    
+        elif (check2Length == 2):
+            e1e3Statement= eval(f'{check[2][0]} == {check[2][1]}')
+        elif (check2Length == 3):
+            e1e3Statement= eval(f'{check[2][0]} == {check[2][1]} == {check[2][2]}')
+    return [e1e2Statement, e2e3Statement, e1e3Statement]
+
 def typeChecker(e1, e2, e3):
-    skip= [[],[],[]]
     global isolatedZ
     isolatedZ= (e1[1]*e3[0]-e1[0]*e3[1])*(e1[2]*e2[0]-e1[0]*e2[2])-(e1[1]*e2[0]-e1[0]*e2[1])*(e1[2]*e3[0]-e1[0]*e3[2])
+    print(isolatedZ)
     equalTo= 0#some formula
-    
     
     #undetermined check
     if (isolatedZ == 0):
@@ -39,47 +88,22 @@ def typeChecker(e1, e2, e3):
         if (equalTo != 0):
             inconsistentHandler(e1, e2, e3)
             return
-        #division by zero prevention
-        for index in range(3):
-            #checks for e2[]
-            if (e2[index]==0):
-                if (e1[index==0]):
-                    skip[0][index]= True
-                else:
-                    skip[0]= True
-            else:
-                skip[0][index]= False
-            #checks for e3[]
-            if (e3[index]==0):
-                if (e2[index]==0):
-                    skip[1][index]= True
-                else:
-                    skip[1]= True
-
-                if (e1[index]==0):
-                    skip[2][index]= True
-                else:
-                    skip[2]= True
-            else:
-                skip[1][index]= False
-                skip[2][index]= False
-
-        
+        e1e2Statement, e2e3Statement, e1e3Statement= preventDivByZero(e1, e2, e3)
         multiples= False
-        if (e1[0] / e2[0] == e1[1] / e2[1] == e1[2] / e2[2]):
+        if (e1e2Statement):
             print('The 1st and the 2nd equation are multiples')
             multiples= True
-        if (e2[0] / e3[0] == e2[1] / e3[1] == e2[2] / e3[2]):
+        if (e2e3Statement):
             print('The 2nd and the 3rd equation are multiples')
             multiples= True
-        if (e1[0] / e3[0] == e1[1] / e3[1] == e1[2] / e3[2]):
+        if (e1e3Statement):
             print('The 1st and the 3rd equation are multiples')
             multiples= True
         if (not multiples):
             print('The equations are linearly dependent')
         else:
             undeterminedHandler(e1, e2, e3)
-        return
+            return
     else:
         determinedHandler(e1, e2, e3)
         return

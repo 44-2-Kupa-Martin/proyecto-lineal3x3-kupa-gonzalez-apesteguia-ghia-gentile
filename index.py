5eeacec81a9d29daa2fbf337df11a6ae2f0801f9
44-2-Kupa-Martin <kupamartinclassroom@gmail.com>
2021-10-07 13:40:24 -0300

    .

--- index.py
+++ index.py
@@ -72,7 +72,7 @@ def type_checker(M):
         if (result[2][3]!=0):
             inconsistent_handler()
             return
-    except:
+    except TypeError:
         pass
     undetermined_handler(result)
     return
